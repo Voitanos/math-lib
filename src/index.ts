88010e3647935a885aa33f86848f23d213a25425
Andrew Connell <me@andrewconnell.com>
2019-02-26 06:20:57 -0600

    update NPM packaging & docs

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 export class MathLib {
+  /**
+   * Adds two numbers together
+   * 
+   * @param operandA First operand to add together.
+   * @param operandB Second operand to add together
+   */
   public add(operandA: number, operandB: number): number {
     return operandA + operandB;
   }
